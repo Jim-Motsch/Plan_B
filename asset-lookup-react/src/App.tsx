d26270ff858fba4d13b998ca930237e57b859192
Jim_Motsch <jimmymotsch@gmail.com>
2026-06-12 21:10:02 -0500

    asset-lookup-react functional

--- asset-lookup-react/src/App.tsx
+++ asset-lookup-react/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+export default function App() {
   const devices = [
     { id: "511168", name: "John", date: "2023", model: "2120" },
     { id: "511169", name: "Joh", date: "2023", model: "2120" },
@@ -9,26 +9,27 @@ function App() {
     { id: "511171", name: "J", date: "2024", model: "2120" },
     { id: "511172", name: "Dan", date: "2024", model: "2120" }
 ];
+    //1. Retain data between redners
+    //2. Trigger React to render the component with new data
     // (state will live here tomorrow — nothing tonight)
-
+    const [query, setQuery] = useState('');
+    const[result,setResult] = useState(null)
     //(event handler function will live here)
-    function handleSearch(deviceId: string){
+    function handleSearch(/*deviceId: string*/){
       for (const device of devices) {
-        if (deviceId === device.id) {
-          return device;
+        if (query === device.id) {
+          setResult(device);
       }
     }
       }
     return (
         <div>
           <label>
-            Asset Lookup: <input name="myInput" />
+            Asset Lookup: <input name="myInput" onChange={e=>setQuery(e.target.value)} />
           </label>
-          <button type="submit">Search</button>
-            {/* a button */}
-            {/* a results area */}
+          <button onClick={handleSearch}>Search</button>
+          {result && <h1>{result.id} - {result.name} - {result.date} - {result.model}</h1>}
+          {/* a results area */}
         </div>
     );
 }
-
-export default App;
